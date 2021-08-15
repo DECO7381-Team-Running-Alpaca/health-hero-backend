b214cd6993a49a6340800bb76cefdaa5319fc90d
Vincent0Lee <vincent0lee95@gmail.com>
2021-08-15 19:27:12 +0800

    Demo for instruction

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use('/teams', teamRouters);
 
 // Delete following
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+  res.send('<h1>Hello World!!!</h1>');
 });
 
 app.listen(port, () => console.log(`Listening on ${port}`));
