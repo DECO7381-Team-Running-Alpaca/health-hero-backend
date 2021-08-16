d4638c1cbfa5a0178db61f15f371f6f46dbf7298
Tony <anicedaytony@outlook.com>
2021-08-16 13:28:59 +1000

    optimize db structure

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,60 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const meal = require('./mealList');
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    user_name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid!');
+        }
+      },
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+      min: 100,
+      max: 250,
+    },
+    weight: {
+      type: Number,
+      required: true,
+      min: 30,
+      max: 250,
+    },
+    preference: {
+      type: Array,
+      default: [],
+    },
+    allergy: {
+      type: Array,
+      default: [],
+    },
+    current_plan: [meal],
+  },
+  { _id: true }
+);
 
 // eslint-disable-next-line func-names
 userSchema.pre('save', async function (next) {
