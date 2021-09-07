03f52d19f792e5deabf2f6c047e3b0aeea2e5d89
Tony <anicedaytony@outlook.com>
2021-09-07 14:09:08 +1000

    controller optimize and meal model build

--- src/models/meal.js
+++ src/models/meal.js
@@ -1,27 +1,41 @@
 const mongoose = require('mongoose');
 
-const mealListSchema = new mongoose.Schema({
+const mealSchema = new mongoose.Schema({
   title: {
-
+    type: String,
+    require: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
   },
   source_url: {
-
+    type: String,
+    trim: true,
   },
   dish_types: [
     {
-
-    }
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!(value in ('breakfast', 'lunch', 'dinner'))) {
+          throw new Error('Dish type must be breakfast, lunch or dinner.');
+        }
+      },
+    },
   ],
   ingredients: [
     {
-
-    }
+      type: String,
+      trim: true,
+    },
   ],
   calories: {
-
+    type: Number,
+    min: 0,
   },
 });
 
-const MealList = mongoose.model('MealList', mealListSchema);
+const MealList = mongoose.model('Meal', mealSchema);
 
 module.exports = MealList;
