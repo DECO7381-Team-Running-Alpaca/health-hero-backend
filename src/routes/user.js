377c0fe5e395f9b2c57542fc8d477689f1740d3d
ziputang <tangzipu19961109@gmail.com>
2021-09-17 15:58:50 +1000

    complete add multiple preferences

--- src/routes/user.js
+++ src/routes/user.js
@@ -16,6 +16,7 @@ const {
   removeAllergy,
   getCurrentUserAllergies,
   generateMealPlan,
+  addmultiplePreference,
 } = require('../controllers/user');
 
 const validator = require('../middlewares/validator');
@@ -74,7 +75,7 @@ router.patch('/users/me', validator, updateCurrentUser);
 router.delete('/users/me', validator, deleteCurrentUser);
 
 // Add a preference to current user by p_name
-router.post('/users/preferences/:p_name', validator, addPreference);
+router.post('/users/preferencesp_name', validator, addPreference);
 
 // Remove a preference from a user
 router.delete('/users/preferences/:p_name', validator, removePreference);
@@ -92,6 +93,10 @@ router.delete('/users/allergies/:a_name', validator, removeAllergy);
 router.get('/users/allergies/', validator, getCurrentUserAllergies);
 
 // Generate meal plan
-router.post('/users/meal', validator, generateMealPlan)
+router.post('/users/meal', validator, generateMealPlan);
+
+// Add multiple preferences
+
+router.post('/users/preferences', validator, addmultiplePreference);
 
 module.exports = router;
