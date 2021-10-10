1e11fc6aa0fc9df45c6105b2d6d83f9a45ea0ace
Vincent0Lee <vincent0lee95@gmail.com>
2021-10-10 17:03:55 +0800

    Fix databse url bug

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Do not turn on UQ VPN (global connect) when connecting with mongo atlas
 exports.connectToDB = () => {
-  const DBurl = process.env.DB_URL_DEV;
+  const DBurl = process.env.DB_URL;
   console.log(`Connecting to ${DBurl}`);
 
   return mongoose.connect(DBurl, {
